e79cf1a3377b16c155c318c1ceb32d23eb895b2b
tmannazy <e.anene@hotmail.com>
2021-07-13 14:06:32 +0100

    Create elements that display on page load

--- src/home.js
+++ src/home.js
@@ -1,20 +1,39 @@
 const homePage = () => {
-    const h1 = document.createElement('h1');
-    const info = document.createElement('p');
-    // const
-    const homePageContainer = document.createElement('div');
+    const h1 = document.createElement('h1'),
+        welcomeDiv = document.createElement('div'),
+        welcomeNote = document.createElement('p'),
+        slogan = document.createElement('p'),
+        orderBtn = document.createElement('button'),
+        locDiv = document.createElement('div'),
+        locDivAddress = document.createElement('p'),
+        locDivList = document.createElement('ul'),
+        operationDays = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'],
+        homePageContainer = document.createElement('main');
 
     h1.textContent = 'kuizin!';
-    info.textContent = 'Welcome to the best restaurant in town with the best kuizins that meet your sought-after needs. Kindly go through our menu for more.';
+    welcomeNote.textContent = 'Welcome to the best restaurant in town with the best kuizins that meet your sought-after needs. Kindly go through our menu for more.';
+    slogan.textContent = 'Cuisines That Satisfy';
+    orderBtn.textContent = 'Order Now';
+    locDivAddress.textContent = '#1 University of Biafra Road, Enugwu, Biafra.'
+    welcomeDiv.className = 'welcome-info';
+    locDiv.className = 'location-info';
 
+    for (let i = 0; i < 2; i++) {
+        let firstGroup, secondGroup;
+        const li = document.createElement('li');
+        if (i === 1) {
+            firstGroup = `${operationDays[4]} - ${operationDays[6]}: 8:00 a.m. - 11:00 p.m.`;
+            li.textContent = firstGroup;
+        } else {
+            secondGroup = `${operationDays[0]} - ${operationDays[3]}: 7:00 a.m. - 9:00 p.m.`;
+            li.textContent = secondGroup;
+        }
+        locDivList.appendChild(li);
+    }
 
-
-
-    homePageContainer.append(h1, info);
-
-
-
-
+    welcomeDiv.append(welcomeNote, slogan, orderBtn);
+    locDiv.append(locDivAddress, locDivList);
+    homePageContainer.append(h1, welcomeDiv, locDiv);
     return homePageContainer;
 }
 
