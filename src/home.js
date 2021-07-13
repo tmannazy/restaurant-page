eb5c87b90bad11f464db6627657ef0ef03e22603
tmannazy <e.anene@hotmail.com>
2021-07-13 01:42:42 +0100

    Create homepage file

--- src/home.js
+++ src/home.js
@@ -1,15 +1,23 @@
-const initialFunc = () => {
+const homePage = () => {
     const h1 = document.createElement('h1');
     const info = document.createElement('p');
-    const img = document.createElement('img');
-    const divC = document.createElement('div');
+    // const
+    const homePageContainer = document.createElement('div');
+
     h1.textContent = 'kuizin!';
     info.textContent = 'Welcome to the best restaurant in town with the best kuizins that meet your sought-after needs. Kindly go through our menu for more.';
-    // img.src = '../src/carolina-marinelli-PkbyxvkGWcU-unsplash.jpg';
-    // divC.append(h1, info);
-    // return divC;
+
+
+
+
+    homePageContainer.append(h1, info);
+
+
+
+
+    return homePageContainer;
 }
 
 export {
-    initialFunc
+    homePage
 }
