8676c6d6aa4dabd89742ac2ad87ae9da62b3991f
tmannazy <e.anene@hotmail.com>
2021-07-13 01:39:31 +0100

    Setup configurations for page modules and plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,31 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
     devtool: 'inline-source-map',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Kuizin!',
+            template: '/src/index.html'
+        }),
+    ],
     module: {
         rules: [
             {
                 test: /\.(png|jpg|jpeg|svg|gif)$/i,
                 type: 'asset/resource'
-            }
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
         ]
     },
 }
