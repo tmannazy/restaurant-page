2a4e475d1700491075e7910ac2c6b30458e343a7
tmannazy <e.anene@hotmail.com>
2021-07-13 01:44:17 +0100

    Create navigation links for the page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log('I just successfully set up my first webpack project :)!');
+import { homePage } from "./home";
+import './style.css';
+
+
+const header = document.createElement('header');
+const nav = document.createElement('nav');
+const ul = document.createElement('ul');
+const navItems = ['home', 'menu', 'contact'];
+navItems.forEach(item => {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    a.setAttribute('href', './' + item + '.js');
+    a.textContent = item;
+    li.appendChild(a);
+    ul.appendChild(li);
+    nav.appendChild(ul);
+    header.appendChild(nav);
+})
+
+const content = document.getElementById('content');
+content.appendChild(homePage());
+body.appendChild(header);
+
+
