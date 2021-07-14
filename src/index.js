6601ba0191075a87930494901a93becab038c35c
tmannazy <e.anene@hotmail.com>
2021-07-14 02:30:40 +0100

    Import the menu module

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
 import { homePage } from "./home";
+import { menuPage } from "./menu";
 import './style.css';
 
 
+const content = document.getElementById('content');
 const header = document.createElement('header');
 const nav = document.createElement('nav');
 const ul = document.createElement('ul');
 const navItems = ['home', 'menu', 'contact'];
+
 navItems.forEach(item => {
     const li = document.createElement('li');
     const a = document.createElement('a');
-    a.setAttribute('href', './' + item + '.js');
+    a.setAttribute('href', './');
     a.textContent = item;
     li.appendChild(a);
     ul.appendChild(li);
     nav.appendChild(ul);
     header.appendChild(nav);
-})
+});
 
-const content = document.getElementById('content');
-content.appendChild(homePage());
-body.appendChild(header);
+// content.append(header, homePage(), menuPage());
+content.append(header, menuPage());
 
 
