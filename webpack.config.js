6348b02347d9d1168ae47309ecdb6392e28e312e
tmannazy <e.anene@hotmail.com>
2021-07-14 12:22:14 +0100

    Add webpack dev server location

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,9 @@ module.exports = {
     mode: 'development',
     entry: './src/index.js',
     devtool: 'inline-source-map',
+    devServer: {
+        contentBase: ',/dist',
+    },
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
