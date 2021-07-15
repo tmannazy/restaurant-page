fba15c29ed4c15aa9bc7bdf914468500f1be20b7
tmannazy <e.anene@hotmail.com>
2021-07-15 19:01:49 +0100

    Refactor code and create a page footer

--- src/index.js
+++ src/index.js
@@ -1,56 +1,69 @@
 import { homePage } from "./home";
 import { menuPage } from './menu';
+import { contactPage } from './contact';
 import './jollof.jpeg';
 import './abacha.jpeg';
 import './beans.jpeg';
 import './style.css';
 
 
+const content = document.getElementById('content'),
+    header = document.createElement('header'),
+    footer = document.createElement('footer'),
+    footerInfo = document.createElement('p'),
+    nav = document.createElement('nav'),
+    ul = document.createElement('ul'),
+    h1 = document.createElement('h1'),
+    navItems = ['home', 'menu', 'contact'];
+
+footerInfo.textContent = 'page created by tmannazy';
+footer.append(footerInfo);
+content.append(header, homePage(), menuPage(), contactPage(), footer);
+
+
 // cacheDOM
-const content = document.getElementById('content');
-const header = document.createElement('header');
-const nav = document.createElement('nav');
-const ul = document.createElement('ul');
-const h1 = document.createElement('h1');
-const navItems = ['home', 'menu', 'contact'];
+const mainContainer = document.querySelector('main'),
+    menuContainer = document.querySelector('.menu-container'),
+    contactContainer = document.querySelector('.contact-container');
 
 
-// functions
+// populate navbar
 navItems.forEach(item => {
-    const li = document.createElement('li');
-    const a = document.createElement('a');
-    a.setAttribute('href', '#');
+    const li = document.createElement('li'),
+        a = document.createElement('a');
     a.textContent = item;
+    h1.textContent = 'kuizin!';
+    a.setAttribute('href', '#');
     li.setAttribute('id', item);
     li.appendChild(a);
     ul.appendChild(li);
     nav.appendChild(ul);
-    h1.textContent = 'kuizin!';
     header.append(h1, nav);
 });
-content.append(header, homePage(), menuPage());
 
-const navPages = Array.from(document.querySelectorAll('li')),
-    mainContainer = document.querySelector('main'),
-    menuContainer = document.querySelector('.menu-container'),
-    contactContainer = document.querySelector('.contact');
+const navPages = Array.from(document.querySelectorAll('li'));
 
 
+// callBack Functions
 const loadPage = () => {
     menuContainer.style.display = 'none';
+    contactContainer.style.display = 'none';
 }
 
 const pageContents = event => {
     const navClicked = event.target.closest('li');
     if (navClicked.id === 'home') {
+        contactContainer.style.display = 'none';
         menuContainer.style.display = 'none';
         mainContainer.style.display = 'block';
     } else if (navClicked.id === 'menu') {
+        contactContainer.style.display = 'none';
         mainContainer.style.display = 'none';
         menuContainer.style.display = 'flex';
     } else if (navClicked.id = 'contact') {
+        contactContainer.style.display = 'block';
         mainContainer.style.display = 'none';
-        menuContainer.style.display = 'flex';
+        menuContainer.style.display = 'none';
     }
 }
 
