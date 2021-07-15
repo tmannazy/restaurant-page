6712fc9a02146a37eaa69d24a96a566d9984005e
tmannazy <e.anene@hotmail.com>
2021-07-15 13:17:01 +0100

    Refactor code and logic to populate page contents

--- src/index.js
+++ src/index.js
@@ -11,7 +11,9 @@ const content = document.getElementById('content');
 const header = document.createElement('header');
 const nav = document.createElement('nav');
 const ul = document.createElement('ul');
+const h1 = document.createElement('h1');
 const navItems = ['home', 'menu', 'contact'];
+
 
 // functions
 navItems.forEach(item => {
@@ -23,24 +25,35 @@ navItems.forEach(item => {
     li.appendChild(a);
     ul.appendChild(li);
     nav.appendChild(ul);
-    header.appendChild(nav);
+    h1.textContent = 'kuizin!';
+    header.append(h1, nav);
 });
 content.append(header, homePage(), menuPage());
 
-const menuContainer = document.querySelector('.menu-container');
-const menuBtn = document.getElementById('menu');
+const homeBtn = document.getElementById('home'),
+    menuBtn = document.getElementById('menu'),
+    contactBtn = document.getElementById('contact'),
+    mainContainer = document.querySelector('main'),
+    menuContainer = document.querySelector('.menu-container');
+
 
 const loadPage = () => {
     menuContainer.style.display = 'none';
 }
 
+const loadHome = () => {
+    menuContainer.style.display = 'none';
+    mainContainer.style.display = 'block';
+
+}
 const loadMenu = () => {
-    content.removeChild(content.childNodes[1]);
+    mainContainer.style.display = 'none';
     menuContainer.style.display = 'flex';
 }
 
 // bindEvents
 document.addEventListener('DOMContentLoaded', loadPage);
+homeBtn.addEventListener('click', loadHome);
 menuBtn.addEventListener('click', loadMenu);
 
 
